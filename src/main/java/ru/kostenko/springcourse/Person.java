003248efd2413e09a25ccd3c6470873173bb0026
Viktor <vvk207@mail.ru>
2021-02-15 13:05:37 +0300

    Adding class Person.java

---
+++ src/main/java/ru/kostenko/springcourse/Person.java
@@ -0,0 +1,31 @@
+package ru.kostenko.springcourse;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
